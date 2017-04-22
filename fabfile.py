11fd905d05ff322ca260d0d6000d27c1a7d02d4b
Da5id2517 <bnesic993@gmail.com>
2017-04-22 16:18:35 +0200

    Fabfile modified.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
-from fabric.api import run
+from fabric.api import run, local
 
 
 def build_n_serve():
+    local('docker build -t da5id2517/seven_tweets .')
+    local('docker push da5id2517/seven_tweets')
+    run('docker stop $(docker ps -q)')
     run('docker pull da5id2517/seven_tweets')
     run('docker run -p 80:5000 da5id2517/seven_tweets')
